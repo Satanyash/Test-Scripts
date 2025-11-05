91a8fda33275bd1db820a206292713369dc8f800
Satyam <khins.emmin@gmail.com>
2025-11-05 19:45:12 +0700

    Small fix

--- lesson_03/lesson_3_task_2.py
+++ lesson_03/lesson_3_task_2.py
@@ -1,11 +1,11 @@
 from smartphone import Smartphone
 
-catalog = [""] * 5
-catalog[0] = Smartphone("Xiaomi", "15T", "+79005553535")
-catalog[1] = Smartphone("Apple", "11 Pro", "+79005553030")
-catalog[2] = Smartphone("Samsung", "Galaxy S5", "+79005552020")
-catalog[3] = Smartphone("Tecno", "Ultra 3", "+79005551515")
-catalog[4] = Smartphone("Pixel", "Super 5", "+79005551010")
+catalog = []
+catalog.append(Smartphone("Xiaomi", "15T", "+79005553535"))
+catalog.append(Smartphone("Apple", "11 Pro", "+79005553030"))
+catalog.append(Smartphone("Samsung", "Galaxy S5", "+79005552020"))
+catalog.append(Smartphone("Tecno", "Ultra 3", "+79005551515"))
+catalog.append(Smartphone("Pixel", "Super 5", "+79005551010"))
 n = len(catalog)
 for i in range(0, n):
     catalog[i].getBrand()
