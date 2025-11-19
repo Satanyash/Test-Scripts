aab568c8755103d54688a726a693596a1eeb0b4c
Satanyash <khins.emmin@gmail.com>
2025-11-19 16:21:29 +0700

    Lesson_06 fix

--- lesson_06/lesson_06_task_3.py
+++ lesson_06/lesson_06_task_3.py
@@ -7,9 +7,14 @@ driver = webdriver.Chrome()
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
 WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape"))
+   EC.all_of(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#compass")),
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#calendar")),
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#award")),
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#landscape"))
+    )
 )
 
-print(driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("src"))
+print(driver.find_element(By.XPATH, "//*[@id='image-container']/img[3]").get_attribute("src"))
 
 driver.quit()
