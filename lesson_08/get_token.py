c1067cb12cee2c0eb57458a408d71443fb39df2e
Satanyash <khins.emmin@gmail.com>
2025-12-07 13:50:11 +0700

    Lesson_08

---
+++ lesson_08/get_token.py
@@ -0,0 +1,18 @@
+import requests
+
+base_url = "https://ru.yougile.com"
+
+def get_token(login, password):
+    authentification = {
+        'login': login,
+        'password': password
+    }
+    company_list = requests.post(base_url+"/api-v2/auth/companies", json=authentification)
+    authentification["companyId"] = company_list.json()["content"][0]["id"]
+    token_response = requests.post(base_url+"/api-v2/auth/keys", json=authentification)
+    return token_response.json()["key"]
+
+# НАДО ПОТОМ УБРАТЬ
+login = "30marnie@comfythings.com"
+password = "p3e-9NK-muy-bf2"
+print("Your token: "get_token(login, password))
